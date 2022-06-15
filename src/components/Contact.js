4f990b71a63202985bd02b4a304fbebaf307a644
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-16 01:08:09 +0530

    Add brand icons to the contact.js file and style the contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,34 @@
 import React from "react";
+import "./Contact.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faGithub,
+  faInstagram,
+  faTwitter,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Contact = () => {
   return (
     <main>
-      <section>Contact</section>
+      <section className="contact-card">
+        <a href="https://www.github.com/azazel-oss" className="contact-field">
+          <FontAwesomeIcon icon={faGithub} className="contact-brand-icon" />
+          <div className="contact-brandname">Github</div>
+        </a>
+        <a href="https://www.twitter.com" className="contact-field">
+          <FontAwesomeIcon icon={faTwitter} className="contact-brand-icon" />
+          <div className="contact-brandname">Twitter</div>
+        </a>
+        <a href="https://www.facebook.com" className="contact-field">
+          <FontAwesomeIcon icon={faFacebook} className="contact-brand-icon" />
+          <div className="contact-brandname">Facebook</div>
+        </a>
+        <a href="https://www.instagram.com" className="contact-field">
+          <FontAwesomeIcon icon={faInstagram} className="contact-brand-icon" />
+          <div className="contact-brandname">Instagram</div>
+        </a>
+      </section>
     </main>
   );
 };
