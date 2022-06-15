16c0c8670664887694655d887b435bd93cbb6c81
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-16 01:07:26 +0530

    Update Home.js file and add styles to the home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,24 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
-  return <main>Home</main>;
+  return (
+    <main className="main__home">
+      <div className="home-description">
+        <span>Find the best *item* and at the most affordable prices.</span>
+        <br />
+        The best there ever was,the best there is, the best there ever will be.
+      </div>
+      <div className="home-cta">
+        <a href="/shopping" className="btn btn-shop-now">
+          Shop now
+        </a>
+        <a href="/contact" className="btn btn-contact-us">
+          Get to know us
+        </a>
+      </div>
+    </main>
+  );
 };
 
 export default Home;
