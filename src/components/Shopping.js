e9f8d95f2caa3c2fe5ce46bd66022f7456012560
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-17 13:41:48 +0530

    Update shopping route, add products to the page and style the page

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,7 +1,26 @@
 import React from "react";
+import "./Shopping.css";
 
-const Shopping = () => {
-  return <main>Shopping</main>;
+const Shopping = (props) => {
+  function addButtonHandler(event) {
+    props.addToCartHandler(event.target.parentNode.dataset.id);
+  }
+  return (
+    <main className="products-container">
+      {props.products.map((product) => (
+        <div key={product.id} data-id={product.id} className="product">
+          <img src={`/assets/images/${product.image}`} alt={product.name} />
+          <div className="product-info">
+            <div className="product-info__name">{product.name}</div>
+            <div className="product-info__price">${product.price}</div>
+          </div>
+          <button onClick={addButtonHandler} className="btn btn-add-cart">
+            Add to Cart
+          </button>
+        </div>
+      ))}
+    </main>
+  );
 };
 
 export default Shopping;
