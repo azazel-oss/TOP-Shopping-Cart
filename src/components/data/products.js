cb29ca7edbabda8093e54a5b7f2b8891835d1dbd
Asad Mahmood <asadmhmd1234@gmail.com>
2022-06-17 13:40:34 +0530

    Add dummy products data for shopping list

---
+++ src/components/data/products.js
@@ -0,0 +1,112 @@
+const products = [
+  {
+    id: "ce4fe9a1-3f14-47e6-b098-ab2ad56620c9",
+    name: "Bang Energy",
+    image: "bang.jpg",
+    price: 3.86,
+  },
+  {
+    id: "9d75ee09-f105-4f27-8556-18b8ffa6257c",
+    name: "B Natural Mixed",
+    image: "b-natural-mixed.jpg",
+    price: 2.54,
+  },
+  {
+    id: "099f95dc-4164-4e55-9a03-eaff68d583c4",
+    name: "Celcius Energy",
+    image: "celcius.jpeg",
+    price: 3.18,
+  },
+  {
+    id: "f2d1f021-351e-4380-9bbf-c10eee1b31b4",
+    name: "Coca Cola",
+    image: "coca-cola.jpg",
+    price: 2.93,
+  },
+  {
+    id: "d778f2d1-6a3d-4768-8a97-e1f8fd9dd7f8",
+    name: "Coke Zero",
+    image: "cokezero.jpeg",
+    price: 2.99,
+  },
+  {
+    id: "33459216-b577-404d-beb2-1c7fbe297333",
+    name: "Diet Coke",
+    image: "dietcoke.jpeg",
+    price: 2.74,
+  },
+  {
+    id: "9ea3c28f-c519-4835-b577-bfe0da8a9426",
+    name: "Dr. Pepper",
+    image: "drpepper.jpeg",
+    price: 2.52,
+  },
+  {
+    id: "43985f3b-33bf-491b-992c-c66b1a08ebbf",
+    name: "Monster Energy",
+    image: "monster.jpg",
+    price: 4.07,
+  },
+  {
+    id: "4a001212-ba5c-4352-9dbf-a5bc19c464d2",
+    name: "Mountain Dew",
+    image: "mtdew.jpg",
+    price: 1.79,
+  },
+  {
+    id: "c512a1ea-3655-47c8-8d30-dccefe03bfeb",
+    name: "Paper Boat Aamras",
+    image: "paper-boat-aamras.png",
+    price: 1.38,
+  },
+  {
+    id: "f102852a-67bd-4535-9de5-0433108683f0",
+    name: "Pepsi",
+    image: "pepsi.jpg",
+    price: 1.85,
+  },
+  {
+    id: "4dee5783-a9ba-4c96-ad21-568fba1661f5",
+    name: "Raw Pressery Mango",
+    image: "raw-mango.jpg",
+    price: 4.24,
+  },
+  {
+    id: "836516cb-f82f-4ff4-bed1-a34614f88bb5",
+    name: "Real Cranberry",
+    image: "real-cranberry.jpg",
+    price: 0.99,
+  },
+  {
+    id: "bf22e769-2867-40e6-b9bc-66a9c0c7b99e",
+    name: "Red Bull Energy",
+    image: "red-bull.jpg",
+    price: 2.55,
+  },
+  {
+    id: "1ef29752-651e-4a65-a787-a75cf6aee8a7",
+    name: "Rockstar Energy",
+    image: "rockstar.jpg",
+    price: 4.79,
+  },
+  {
+    id: "ca856fc8-00c9-4f3e-a45b-2dedcd189a49",
+    name: "Sprite",
+    image: "Sprite.jpg",
+    price: 2.33,
+  },
+  {
+    id: "d70aedf1-a70f-474e-8b92-2504833ef440",
+    name: "Tropicana Apple",
+    image: "tropicana-apple.jpg",
+    price: 2.48,
+  },
+  {
+    id: "000ce12a-30a7-48ed-92f4-e7a2bf45779d",
+    name: "Xyience Energy",
+    image: "xyience.jpeg",
+    price: 4.84,
+  },
+];
+
+export default products;
